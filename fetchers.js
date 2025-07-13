85997c8c7d39102f084314c1bc248110c97df6b1
plop14 <70171493+plero75@users.noreply.github.com>
2025-07-13 13:24:39 +0200

    Update fetchers.js

--- fetchers.js
+++ fetchers.js
@@ -1,39 +1,60 @@
 import { CONFIG } from "./config.js";
 import { parseStop, parseJourney } from "./parser.js";
 
-/* Appel générique avec proxy + headers */
-async function call(url){
-  const final = CONFIG.proxy ? CONFIG.proxy+encodeURIComponent(url) : url;
-  const res   = await fetch(final,{headers:CONFIG.headers});
-  if(!res.ok) throw new Error(`HTTP ${res.status} on ${url}`);
-  return res.json();
+/* ------------------------------------------------------------------
+ *  Wrapper fetch commun : ajoute le proxy + encode l’URL cible
+ *  (aucun header apikey n’est ajouté, le proxy s’en charge)
+ * -----------------------------------------------------------------*/
+async function call(targetUrl) {
+  const url = CONFIG.proxy
+    ? CONFIG.proxy + encodeURIComponent(targetUrl)
+    : targetUrl;
+
+  const resp = await fetch(url);          // ← pas de headers
+  if (!resp.ok) throw new Error(`HTTP ${resp.status} sur ${targetUrl}`);
+  return resp.json();
 }
 
-/* StopMonitoring + GeneralMessage pour une station ----------------*/
-export async function fetchStation(stopRef,lineRef){
-  /* 1) prochains passages */
-  const stopUrl = `https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring`
-                + `?MonitoringRef=${stopRef}`;
-  const stopJson = await call(stopUrl);
+/* ------------------------------------------------------------------
+ *  Récupère les prochains passages + éventuelle alerte perturbation
+ * -----------------------------------------------------------------*/
+export async function fetchStation(stopRef, lineRef) {
+  /* 1) Prochains départs (StopMonitoring) */
+  const stopUrl =
+    "https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring" +
+    `?MonitoringRef=${stopRef}`;
+
+  const stopJson   = await call(stopUrl);
   const departures = parseStop(stopJson);
 
-  /* 2) perturbations éventuelles */
-  let alert="";
-  if(lineRef){
-    const msgUrl = `https://prim.iledefrance-mobilites.fr/marketplace/general-message`
-                 + `?LineRef=${lineRef}`;
-    try{
-      const gm   = await call(msgUrl);
-      alert = gm?.Siri?.ServiceDelivery?.GeneralMessageDelivery?.[0]
-              ?.InfoMessage?.[0]?.Content?.MessageText || "";
-    }catch{/* silencieux */}
+  /* 2) Message perturbation (GeneralMessage) */
+  let alert = "";
+  if (lineRef) {
+    const gmUrl =
+      "https://prim.iledefrance-mobilites.fr/marketplace/general-message" +
+      `?LineRef=${lineRef}`;
+
+    try {
+      const gmJson = await call(gmUrl);
+      alert =
+        gmJson?.Siri?.ServiceDelivery?.GeneralMessageDelivery?.[0]
+          ?.InfoMessage?.[0]?.Content?.MessageText || "";
+    } catch {
+      /* Pas de message ➜ on ignore silencieusement */
+    }
   }
-  return {departures,alert};
+
+  return { departures, alert };
 }
 
-/* Détail gares RER A pour un VehicleJourney -----------------------*/
-export async function fetchJourney(journeyRef){
-  const url = `https://prim.iledefrance-mobilites.fr/marketplace/vehicle-journey/${journeyRef}`;
+/* ------------------------------------------------------------------
+ *  Détail des gares restantes pour un VehicleJourney (RER A…)
+ * -----------------------------------------------------------------*/
+export async function fetchJourney(journeyRef) {
+  const url =
+    "https://prim.iledefrance-mobilites.fr/marketplace/vehicle-journey/" +
+    journeyRef;
+
   const json = await call(url);
   return parseJourney(json);
 }
